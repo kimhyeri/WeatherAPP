492a8d528ea7ca9a7c1d624602a35c4860963f2a
hyeri <hyer1k@naver.com>
2019-07-31 20:10:51 +0900

    Create CurrentWeather Page

---
+++ WeatherAPP/WeatherAPP/Controller/CurrentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentViewController.swift
+//  WeatherAPP
+//
+//  Created by hyeri kim on 31/07/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import UIKit
+
+class CurrentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
