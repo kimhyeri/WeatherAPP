30dcda654501a844c9752cc2c632b851b9ede3cd
hyeri <hyer1k@naver.com>
2019-07-31 17:59:52 +0900

    Create WeatherList tableView and cell

---
+++ WeatherAPP/WeatherAPP/View/WeatherListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherListTableViewCell.swift
+//  WeatherAPP
+//
+//  Created by hyeri kim on 31/07/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension WeatherListTableViewCell: CellReusable {}
