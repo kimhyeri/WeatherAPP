e57a3fa6bd0c766e73ec50883c966da77afc6e1a
hyeri <hyer1k@naver.com>
2019-08-01 17:56:55 +0900

    Setup SearchCitiesViewController UI

--- WeatherAPP/WeatherAPP/Controller/SearchCitiesViewController.swift
+++ WeatherAPP/WeatherAPP/Controller/SearchCitiesViewController.swift
@@ -11,18 +11,43 @@ import UIKit
 class SearchCitiesViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            self.searchBar.becomeFirstResponder()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViewController()
     }
 
+    private func setupViewController() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+    }
 }
 
-// MARK: SearchBar
+// MARK: TableView Delegate and DataSource 
+extension SearchCitiesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+}
+
+// MARK: SearchBar Delegate
 extension SearchCitiesViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
 }
