69cbb32e85b907503bab5a17a42643d08276048c
hyeri <hyer1k@naver.com>
2019-08-03 03:12:50 +0900

    Create EndPoint Seperate networking function

---
+++ WeatherAPP/WeatherAPP/Networking/BaseURL.swift
@@ -0,0 +1,13 @@
+//
+//  BaseURL.swift
+//  WeatherAPP
+//
+//  Created by hyeri kim on 03/08/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import Foundation
+
+struct BaseURL {
+    static let weatherURL = "https://api.openweathermap.org/data/2.5/weather" 
+}
