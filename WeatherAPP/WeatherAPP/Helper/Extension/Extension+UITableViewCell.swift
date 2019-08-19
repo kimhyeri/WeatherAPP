a08b8fb91361d37e984825fc4c89184e8fdcea88
hyeri <hyer1k@naver.com>
2019-08-19 19:48:51 +0900

    #6 Refactoring - Extension UITableView Create registerNib protocol

--- WeatherAPP/WeatherAPP/Helper/Extension/Extension+UITableViewCell.swift
+++ WeatherAPP/WeatherAPP/Helper/Extension/Extension+UITableViewCell.swift
@@ -17,6 +17,14 @@ extension UITableView {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Unable Dequeue Reusable")
         }
+    
         return cell
+    }
+    
+    func register<T: UITableViewCell>(_: T.Type) where T: NibLoadable {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        
+        register(nib, forCellReuseIdentifier: T.reuseIdentifier)
     }
 }
